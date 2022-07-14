c2bd73b3993e8e995666c74dfd66c0f0f5f0928d
Pablo Machado <pablohms1@gmail.com>
2022-07-13 22:46:49 -0300

    teste

--- src/js/sounds.js
+++ src/js/sounds.js
@@ -2,7 +2,10 @@ export default function Sounds() {
   const bgBonfire = new Audio('../../public/audio/Lareira.wav')
   const bgWood = new Audio('../../public/audio/Floresta.wav')
   const bgRain = new Audio('../../public/audio/Chuva.wav')
-  const bgCoffee = new Audio('../../public/audio/Cafeteria.wav')
+  // const bgCoffee = new Audio('../../public/audio/Cafeteria.wav')
+  const bgCoffee = new Audio(
+    'https://github.com/henriquessf/pomodoreTimerNew/blob/main/public/audio/Cafeteria.wav'
+  )
 
   bgBonfire.loop
   bgWood.loop
